e65cc186f454f74467a246f05c902743ba1b9297
jonahkula <jonahkula@gmail.com>
2021-04-17 15:58:28 -0700

    set up chat screen ui

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -65,7 +65,21 @@ function Sidebar() {
 
 export default Sidebar;
 
-const Container = styled.div``;
+const Container = styled.div`
+  flex: 0.45;
+  border-right: 1px solid whitesmoke;
+  height: 100vh;
+  min-width: 300px;
+  max-width: 350px;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  /* IE and Edge */
+  -ms-overflow-style: none;
+  /* Firefox */
+  scrollbar-width: none;
+`;
 
 const IconsContainer = styled.div``;
 
